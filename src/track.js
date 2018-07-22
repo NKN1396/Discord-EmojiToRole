c1a46669f87386b5d4686bf1e2bcffefb7a26015
NKN1396 <someone.took.nkn1396@gmail.com>
2018-07-22 16:38:33 +0200

    Fixed everything related to "messageReactionAdd"

--- src/track.js
+++ src/track.js
@@ -1,3 +1,8 @@
+/**
+ * Returns a unique discriminator string that can be used to distinguish between custom emojis.
+ * @param {Object} emoji The emoji used as the reaction to the message.
+ * @returns {String} The discriminator.
+ */
 function getEmojiDiscriminator(emoji) {
 	if (emoji.id) {
 		return `${emoji.name}:${emoji.id}`;
@@ -16,54 +21,75 @@ module.exports = function(client, config) {
 	client
 		.on("messageReactionAdd", (messageReaction, user) => {
 			//Bot should not react to its own reactions.
-			//console.log("---\nMessage reaction added");
 			if (user == client.user) return;
-			//console.log("Not from me");
 			var member = messageReaction.message.guild.members.get(user.id);
-			//console.log("Member is " + member.displayName);
 			var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji);
-			//console.log("Emoji is " + emojiDiscriminator);
 			(async () => {
-				console.log("Entered ASYNC");
 				for (var bundle of config) {
 					if (bundle.channel != messageReaction.message.channel.id) continue;
-					console.log("Message is in " + messageReaction.message.channel.name);
 					var rolesToAdd = [];
 					var rolesToRemove = [];
 					for (var reaction of bundle.reactions) {
-						//console.log("DISCRIM: " + emojiDiscriminator);
-						//console.log("REACTION: " + require("util").inspect(reaction, {depth: 3}));
 						if (emojiDiscriminator == reaction.emoji) {
 							rolesToAdd.push.apply(rolesToAdd, reaction.roles); //Prototyping the push function, might be buggy
-							console.log("Queued (add) " + reaction.roles);
-						} else {
+						} else if (bundle.disjoint) {
+							//Roles shall be handled mutually exclusive
 							rolesToRemove.push.apply(rolesToRemove, reaction.roles);
-							console.log("Queued (remove) " + reaction.roles);
 						}
 					}
-					console.log("Add: " + rolesToAdd);
-					console.log("Tmp: " + rolesToRemove);
-					//Remove all roles on the "adding" list from the "removal" list
-					rolesToRemove.filter((role) =>
+					await member.addRoles(rolesToAdd.filter((role) =>
+						//Only add roles that the member does not yet have
+						(!member.roles.get(role))
+					));
+					//Check to see if roles are handled mutually eclusive
+					if (!bundle.disjoint) continue;
+					//Make sure none of the roles on the "add" list get removed again
+					await member.removeRoles(rolesToRemove.filter((role) =>
+						//Member already has role that is about to be removed
+						(member.roles.get(role)) &&
+						//Role about to be removed is not on the whitelist
 						(!rolesToAdd.includes(role))
-					);
-					console.log("Rem: " + rolesToRemove);
-					await member.addRoles(rolesToAdd);
-					if (bundle.disjoint) await member.removeRoles(rolesToRemove);
-
+					));
+					await messageReaction.remove(user);
 					//Don't use this, or otherwise there will only be a single message per channel.
 					//break;
 				}
 			})();
-		});
-
-	/*
+		})
+		/*
 		.on("messageReactionRemove", (messageReaction, user) => {
-
 			//Bot should not react to its own reactions.
-			if(user == client.user) return;
-			var member = bot.guilds.get(ID_guild).members.get(user.id);
+			if (user == client.user) return;
+			var member = messageReaction.message.guild.members.get(user.id);
 			var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji);
+
+			(async () => {
+				for (var bundle of config) {
+					if (bundle.channel != messageReaction.message.channel.id) continue;
+					var rolesToAdd = [];
+					var rolesToRemove = [];
+					for (var reaction of bundle.reactions) {
+						if (emojiDiscriminator == reaction.emoji) {
+							rolesToAdd.push.apply(rolesToAdd, reaction.roles); //Prototyping the push function, might be buggy
+						} else if (bundle.disjoint) {
+							//Roles shall be handled mutually exclusive
+							rolesToRemove.push.apply(rolesToRemove, reaction.roles);
+						}
+					}
+					await member.addRoles(rolesToAdd);
+					//Check to see if roles are handled mutually eclusive
+					if(!bundle.disjoint) continue;
+					//Make sure none of the roles on the "add" list get removed again
+					rolesToRemove.filter((role) =>
+						(!rolesToAdd.includes(role))
+					);
+					await member.removeRoles(rolesToRemove);
+					await messageReaction.remove(user);
+					//Don't use this, or otherwise there will only be a single message per channel.
+					//break;
+				}
+			})();
+
 			if(emojiDiscriminator == reaction_1)
 			{
 				if(member.roles.get(ID_role_1)){
@@ -78,6 +104,6 @@ module.exports = function(client, config) {
 					console.log("2 REMOVED");
 				}
 			}
-		});
-		*/
+		})//*/
+	;
 };
