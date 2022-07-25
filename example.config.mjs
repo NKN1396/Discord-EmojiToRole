c52763d7065733f979e315b799d0805919d876f7
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-25 06:17:34 +0200

    Rename config.json to config.mjs

---
+++ example.config.mjs
@@ -0,0 +1,23 @@
+const config = [
+  {
+    message: '446409003136712726',
+    channel: '414617785415630855',
+    disjoint: false,
+    reactions: [
+      {
+        emoji: '🤔',
+        roles: [
+          '446783613439311882'
+        ]
+      },
+      {
+        emoji: 'b26:441046394493599764',
+        roles: [
+          '446783667084197910'
+        ]
+      }
+    ]
+  }
+]
+
+export default config
