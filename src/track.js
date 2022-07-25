007cca803435f5a33f1dada78c483eb8668b30e4
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-25 06:31:47 +0200

    Refactor track.js
    This one is a handful. The code got WAY too messy to maintain, so I decided to give it an overhaul. Should make everything easier to read and maintain.

--- src/track.js
+++ src/track.js
@@ -1,91 +1,104 @@
+let config
+
 /**
- * Returns a unique discriminator string that can be used to distinguish between custom emojis.
- * @param {Object} emoji The emoji used as the reaction to the message.
- * @returns {String} The discriminator.
+ * 
+ * @param {*} messageReaction 
+ * @param {*} reactions 
  */
-function getEmojiDiscriminator(emoji) {
-	if (emoji.id) {
-		return `${emoji.name}:${emoji.id}`;
-	} else {
-		return emoji.name;
-	}
+async function setDisjointRoles (messageReaction, reactions) {
+  let rolesToRemove = []
+  const rolesToAdd = []
+
+  for (const reaction of reactions) {
+    if (reaction.emoji === messageReaction.emoji.id) {
+      rolesToAdd.concat(reaction.roles)
+      continue
+    }
+
+    rolesToRemove = [...new Set([...rolesToRemove, ...reaction.roles])]
+  }
+
+  const newRoles = []
+  messageReaction.message.member.roles.set(newRoles)
+}
+
+/**
+ * 
+ * @param {*} messageReaction The reaction that got added
+ * @param {*} user The user that added the reaction
+ */
+async function handleReactionAdd (messageReaction, user) {
+  // Bot should not react to its own reactions.
+  if (messageReaction.me) return
+
+  // Check if message is among the ones being tracked
+  const scheme = config.get(messageReaction?.message?.id)
+  if (scheme === undefined) return
+
+  
+
+  for (const { message, reactions, disjoint } of config) {
+    if (message !== ) continue
+
+    if (disjoint) {
+      // Add one role, remove all other
+      setDisjointRoles(messageReaction, reactions)
+    } else {
+      // Only add one role
+      // member.add(reaction.roles)
+    }
+  }
+}
+
+/**
+ * 
+ * @param {*} messageReaction The reaction that got removed
+ * @param {*} user The user that removed the reaction
+ */
+async function handleReactionRemove (messageReaction, user) {
+  // Bot should not react to its own reactions.
+  if (messageReaction.me) return
+
+  const member = messageReaction.message.guild.members.cache.get(user.id)
+  const emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji)
+
+  for (const { disjoint, channel, reactions } of config) {
+    // * Make sure we're not in "disjoint" mode
+    if (disjoint) continue
+    if (channel != messageReaction.message.channel.id) continue
+    const rolesToKeep = []
+    const rolesToRemove = []
+    for (const { emoji, roles } of reactions) {
+      if (emojiDiscriminator == emoji) {
+        // * Add to removal list
+        rolesToRemove.push.apply(rolesToRemove, roles)
+      } else {
+        // * List of all other roles that should be kept
+        rolesToKeep.push.apply(rolesToKeep, roles)
+      }
+    }
+    rolesToRemove.filter((role) =>
+    // * Make sure role that is about to be removed is not part of another emoji
+      (!rolesToKeep.includes(role)) &&
+      // * Make sure member actually has role
+      (member.roles.cache.get(role))
+    )
+    await member.removeRoles(rolesToRemove)
+      .catch(error => console.error(error))
+  }
 }
 
 /**
  * Fetches all messages that need to be tracked into the cache. Makes sure each message is having the proper reactions attached.
- * @param {*} client The bot client.
- * @param {*} config The config file.
+ * @param {*} client The bot client
+ * @param {*} config The config file
  */
-module.exports = function(client, config) {
-	client
-		.on("messageReactionAdd", (messageReaction, user) => {
-			// * Bot should not react to its own reactions
-			if (user == client.user) return;
-			var member = messageReaction.message.guild.members.cache.get(user.id);
-			var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji);
-			(async () => {
-				for (var { channel, reactions, disjoint } of config) {
-					if (channel != messageReaction.message.channel.id) continue;
-					var rolesNew = [];
-					for(var role of member.roles.cache.keys()){
-						rolesNew.push(role);
-					}
-					var rolesWhitelist = [];
-					var rolesBlacklist = [];
-					for (var { emoji, roles } of reactions) {
-						if (emojiDiscriminator == emoji) {
-							rolesWhitelist.push.apply(rolesWhitelist, roles); // * Prototyping the push function, might be buggy
-						}
-						rolesBlacklist.push.apply(rolesBlacklist, roles);
-					}
-					// * Check to see if roles are handled mutually exclusive
-					if (disjoint) {
-						rolesNew = rolesNew.filter((role) =>
-							//Remove role if found on watchlist
-							(!rolesBlacklist.includes(role))
-						);
-					}
-					rolesNew.push.apply(rolesNew, rolesWhitelist);
-					// * Make sure none of the roles on the "add" list get removed again
-					await member.roles.set(rolesNew)
-						.catch(error => console.error(error));
-					if (disjoint) await messageReaction.users.remove(user)
-						.catch(error => console.error(error));
-				}
-			})();
-		})
-
-		.on("messageReactionRemove", (messageReaction, user) => {
-			// * Bot should not react to its own reactions.
-			if (user == client.user) return;
-			var member = messageReaction.message.guild.members.cache.get(user.id);
-			var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji);
-			(async () => {
-				for (var { disjoint, channel, reactions } of config) {
-					// * Make sure we're not in "disjoint" mode
-					if (disjoint) continue;
-					if (channel != messageReaction.message.channel.id) continue;
-					var rolesToKeep = [];
-					var rolesToRemove = [];
-					for (var { emoji, roles } of reactions) {
-						if (emojiDiscriminator == emoji) {
-							// * Add to removal list
-							rolesToRemove.push.apply(rolesToRemove, roles);
-						} else {
-							// * List of all other roles that should be kept
-							rolesToKeep.push.apply(rolesToKeep, roles);
-						}
-					}
-					rolesToRemove.filter((role) =>
-						// * Make sure role that is about to be removed is not part of another emoji
-						(!rolesToKeep.includes(role)) &&
-						// * Make sure member actually has role
-						(member.roles.cache.get(role))
-					);
-					await member.removeRoles(rolesToRemove)
-						.catch(error => console.error(error));
-				}
-			})();
-		});
-
-};
+export default function (client, _config) {
+  if(config !== undefined) {
+    console.error('The tracker has already been initiated and is currently working.')
+  }
+  config = _config
+  client
+    .on('messageReactionAdd', handleReactionAdd)
+    .on('messageReactionRemove', handleReactionRemove)
+}
