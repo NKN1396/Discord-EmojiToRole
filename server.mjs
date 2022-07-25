1153f955a9d2960eb268f5d078f7716b696f8356
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-25 06:28:58 +0200

    Rename index.js to server.mjs

---
+++ server.mjs
@@ -0,0 +1,31 @@
+// External dependencies
+import { Client } from 'discord.js'
+
+// Internal dependencies
+import parseConfig from './src/restructureConfig.mjs'
+import fetchMessages from './src/load.js'
+//import track from './src/track.js'
+
+// Configuration
+import config from './config.mjs'
+//import token from './token.json'
+
+/*
+const bot = new Client()
+fetchMessages(bot, config)
+track(bot, config)
+
+bot.login(token)
+  .then(() => {
+    console.log(`Logged in as ${bot.user.tag}`)
+  })
+  .catch(error => {
+    console.error('There was an error while trying to log in.')
+    console.error(error)
+  })
+*/
+
+const messageScheme = parseConfig(config)
+console.log(messageScheme)
+
+
