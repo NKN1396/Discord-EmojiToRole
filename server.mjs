584855e4b2b18316dfd4927d3d4aa5b120a8ad91
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-26 06:09:19 +0200

    Refactor checkMessages.mjs
    Bot now only checks for reactions instead of caching all messages.

--- server.mjs
+++ server.mjs
@@ -9,20 +9,26 @@ import trackReactions from './src/trackReactions.mjs'
 // Configuration
 import CONFIG from './config.mjs'
 import TOKEN from './token.mjs'
+import checkMessages from './src/checkMessages.mjs'
 
 const bot = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember, Partials.User]
 })
+
 const messageScheme = parseConfig(CONFIG)
-// fetchMessages(bot, config)
+
+// Start tracking messages by adding event listeners
 trackReactions(bot, messageScheme)
 
 bot.login(TOKEN)
   .then(() => {
     console.log(`Logged in as ${bot.user.tag}`)
+
+    // Check all messages if the bot has still reacted to them
+    checkMessages(bot, messageScheme)
   })
   .catch(error => {
-    console.error('There was an error while trying to log in.')
+    console.error('There was an error while trying to log in')
     console.error(error)
   })
--- src/checkMessages.mjs
+++ src/checkMessages.mjs
@@ -1,35 +1,43 @@
 /**
- * Makes sure each message is having the proper reactions attached.
+ * Makes sure each message is having the proper reactions attached. This should only be called once the client ist ready.
  * @param {*} client The bot client
  * @param {*} config The config scheme
  */
-export default function (client, config) {
-  client
-    .on('ready', async function () {
-      console.log('Fetching messages')
-      let fetchedMessageCount = 0
-      for (const { channel, message: messageId, reactions } of config) {
-        const message = await client.channels.cache.get(channel).messages.fetch(messageId)
-          .catch(error => console.error(error))
-        if (!message) continue
-        ++fetchedMessageCount
-        for (let { emoji } of reactions) {
-          emoji = cleanEmojiDiscriminator(emoji)
-          const messageReaction = message.reactions.cache.get(emoji)
-          if (!messageReaction) {
-            await message.react(emoji)
-              .catch(console.error())
-            // No fetch necessary since no prior existing reactions.
-          } else {
-            if (!messageReaction.me) {
-              // Fetch each reaction into cache to keep track of them
-              messageReaction.users.fetch({ limit: 50 })
-              await message.react(emoji)
-                .catch(console.error())
-            }
-          }
-        }
-      }
-      console.log(`Done fetching ${fetchedMessageCount} message(s).`)
-    })
+export default async function (client, config) {
+  console.log('Checking all messages')
+
+  for (const [messageId, messageScheme] of config) {
+    // Fetch channel
+    const channelId = messageScheme.channel
+    let channel
+    try {
+      channel = await client.channels.fetch(channelId)
+    } catch (error) {
+      console.error(`Error fetching channel with ID ${channelId}. Does the channel still exist? Does the bot have access to it?`)
+      console.error(error)
+      return
+    }
+
+    // Fetch message
+    let message
+    try {
+      message = await channel.messages.fetch(messageId)
+    } catch (error) {
+      console.error(`Error fetching message with ID ${messageId}. Does the message still exist? Does the bot have access to it?`)
+      console.error(error)
+      return
+    }
+
+    // Add missing emojis
+    const emojis = messageScheme.reactions.keys()
+    for (const emoji of emojis) {
+      await message.react(emoji)
+        .catch(error => {
+          console.error(`Error reacting to message with ID ${messageId} with emoji ${emoji}.`)
+          console.error(error)
+        })
+    }
+  }
+
+  console.log('Done checking messages')
 }
