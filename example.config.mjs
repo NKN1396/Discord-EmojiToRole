24013a71ea9677444013cef1204e932d2c4af8a4
NKN1396 <someone.took.nkn1396@gmail.com>
2022-07-26 10:08:55 +0200

    Add second message to example

--- example.config.mjs
+++ example.config.mjs
@@ -12,6 +12,22 @@ const config = [
         roles: ['1001065583422492794']
       }
     ]
+  }, {
+    message: '1001392404370296962',
+    channel: '414617785415630855',
+    disjoint: true,
+    reactions: [
+      {
+        emoji: 'blushvara:834443926974627860',
+        roles: ['1001393500849446953']
+      }, {
+        emoji: 'a:image06:1000141181377859627', // Custom animated guild emoji
+        roles: ['1001393677685510154']
+      }, {
+        emoji: '%F0%9F%99%82',
+        roles: ['1001393583896674385']
+      }
+    ]
   }
 ]
 
